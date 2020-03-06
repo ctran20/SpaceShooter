f9ea2c6c009a9888a6d859d591d64f5ef4c4e33a
Cat Tran <54409102+ctran20@users.noreply.github.com>
2020-03-06 18:00:27 -0500

    Set Up Splash Screen & Ship Model

---
+++ SpaceShooter/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadGame", 2f);
+    }
+
+    // Update is called once per frame
+    void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
